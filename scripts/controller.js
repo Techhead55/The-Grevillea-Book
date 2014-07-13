bac0495df65a6c81dfca43d3179c98945d4211fc
techhead55 <techhead55@gmail.com>
2014-07-13 21:03:13 +1000

    Genre specific story lists partially implemented

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,13 +2,22 @@ function setSlide(i){
     $("#container").animate({
         left: ""+(-(window.innerWidth*i))
     }, 600 );
+    window.slide = i;
     history.pushState(null, null, "?slide="+i);
 }
+function setGenre(i){
+    history.pushState(null, null, "?slide="+slide+"&genre="+i);
+    window.genre = i;
+}
 function renderData(){
-    for (genre in data.genres){
+    for (genreElement in data.genres){
         var element = document.createElement("div");
-        element.innerHTML = data.genres[genre].name;
+        element.innerHTML = data.genres[genreElement].name;
         element.className = "menu";
+        element.onclick = function(){
+            setGenre(genreElement);
+            setSlide(2);
+        };
         slides[1].contentDocument.getElementById("menu-items").appendChild(element);
     }
 };
@@ -29,7 +38,14 @@ function init(){
     slides[1].contentDocument.getElementById("back").onclick = function(){
         setSlide(0);
     };
+    slides[2].contentDocument.getElementById("back").onclick = function(){
+        setSlide(1);
+    };
+    slides[3].contentDocument.getElementById("back").onclick = function(){
+        setSlide(2);
+    };
     setSlide(getParam("slide"));
+    setGenre(getParam("genre"));
     loadData();
 };
 function size(){
