a53fd493cf4a46a4f8c0ae4dfdf5d3a91e45fdd0
techhead55 <techhead55@gmail.com>
2014-07-13 20:44:48 +1000

    Genre list now loads from data file and there is now a back arrow implemented in the genre menu.

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,12 +4,33 @@ function setSlide(i){
     }, 600 );
     history.pushState(null, null, "?slide="+i);
 }
+function renderData(){
+    for (genre in data.genres){
+        var element = document.createElement("div");
+        element.innerHTML = data.genres[genre].name;
+        element.className = "menu";
+        slides[1].contentDocument.getElementById("menu-items").appendChild(element);
+    }
+};
+function loadData(){
+    var xhr = new XMLHttpRequest;
+    xhr.open("GET", "data.json", true);
+    xhr.send();
+    xhr.onload = function(){
+        window.data = JSON.parse(xhr.responseText);
+        renderData();
+    };
+}
 function init(){
     window.slides = document.getElementsByClassName("slide");
     slides[0].contentDocument.onclick = function(){
         setSlide(1);
     };
+    slides[1].contentDocument.getElementById("back").onclick = function(){
+        setSlide(0);
+    };
     setSlide(getParam("slide"));
+    loadData();
 };
 function size(){
     for (var i=0; i<slides.length; i++){
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,6 @@
 function getParam(key){
-    return window.location.search.split(/[&?]+/).slice(1).filter(function(v, i, o){
+    var val = window.location.search.split(/[&?]+/).slice(1).filter(function(v, i, o){
         if (v.split("=")[0]===key) return v;
-    }, this)[0].split("=")[1];
+    }, this)[0];
+    return val ? val.split("=")[1] : null;
 }
